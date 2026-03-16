aae016d226b38d3f3e8dc7b2200a8d22416540fc
Jack Kibort <jkibort928@gmail.com>
2026-03-16 03:01:08 -0700

    forgot title for forgot

--- frontend/src/pages/Forgot.tsx
+++ frontend/src/pages/Forgot.tsx
@@ -11,7 +11,7 @@ export default function Forgot() {
 
     <div className="mx-auto max-w-4xl px-4 py-12 sm:px-6 lg:px-8">
       <h1 className="font-display text-3xl font-bold text-brand-900">
-        Welcome Back!
+        Forgot Password?
       </h1>
 
       {/* Input & FormField */}
