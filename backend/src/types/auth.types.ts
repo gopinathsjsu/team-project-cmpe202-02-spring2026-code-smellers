f13522fd3bb0934f378565a02367fedefb6d6f3e
Isaiah Callano <isaiah.g.callano@gmail.com>
2026-03-30 13:48:27 -0700

    changed register form fields, names, and optionality modifiers to better match Supabase schema. update users table type from int to uuid/string. add test for registerUser.

--- backend/src/types/auth.types.ts
+++ backend/src/types/auth.types.ts
@@ -1,6 +1,6 @@
 export interface RegisterRequestBody {
-  email?: string;
-  password?: string;
-  displayName?: string;
-  role?: "attendee" | "organizer" | "admin";
+  email: string;
+  password: string;
+  displayName: string;
+  is_admin: boolean;
 }
