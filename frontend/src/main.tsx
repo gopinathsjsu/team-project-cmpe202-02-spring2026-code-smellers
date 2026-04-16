32ad459b9204835c9b867b43badff5b28faf2c94
Isaiah Callano <isaiah.g.callano@gmail.com>
2026-04-15 17:02:25 -0700

    add AuthProvider and wrap entire app with it

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./styles/globals.css";
 import App from "./App.tsx";
+import { AuthProvider } from "./auth/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 );
